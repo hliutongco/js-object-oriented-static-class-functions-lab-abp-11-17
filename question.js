f9c59616f3b36f183e726b391136a47646c75b1c
Helen Liutongco <bellpickle@gmail.com>
2017-11-12 07:35:12 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -5,13 +5,19 @@ class Question{
     this.content=content;
     this.save();
   }
-  
+
   save(){
     this.constructor._All.push(this);
   }
-  
+
   static All(){
     return this._All;
+  }
+
+  static Find(id){
+    return this.All().filter(function(questions){
+      return this._All.indexOf(questions) === id-1
+    })
   }
 }
 
