478d453f91d5d23c2d122f87906e3e8e3e579d12
Helen Liutongco <bellpickle@gmail.com>
2017-11-12 08:37:38 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,25 @@
 'use strict';
+class Question{
+
+  constructor(content){
+    this.content=content;
+    this.save();
+  }
+
+  save(){
+    this.constructor._All.push(this);
+  }
+
+  static All(){
+    return this._All;
+  }
+
+  static Find(id){
+    const test= this._All.filter(function(questions,index){
+      return index===id-1;
+    })
+    return test[0];
+  }
+}
+
+Question._All=[];
